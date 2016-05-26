ffbecf59c12c25bae3c63a66df29316e80d122d6
Nick O <debuggeek@gmail.com>
2016-05-25 22:50:04 -0500

    add aws deployment script

---
+++ admin/awsDeploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+src=~/uploads
+target=~/fivestone/
+
+echo "src set to $src"
+echo "target set to $target"
+
+echo "removing existing $target"
+rm -rf $target
+
+echo "recreating $target"
+mkdir $target
+
+echo "copying base dir over"
+cp $src/*.php $target
+cp $src/*.css $target
+cp $src/*.html $target
+
+echo "copying library over"
+cp -r $src/library $target/library
+
+
+cp -r $src/resources $target/resources
+cp -r $src/MPDF56 $target/MPDF56
+cp -r $src/services $target/services
+
+mkdir $target/upload
+
+cp ~/accountinfo.php $target/library/
