7edb47e9a789b5e3d9883925db54a569bc3510c5
Nick O <debuggeek@gmail.com>
2019-05-11 13:31:00 -0500

    Fix date calc in batchwatchdog

--- admin/batchwatchdog.sh
+++ admin/batchwatchdog.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 LOCKFILE=~/batchwatchdog.lock
-
+d=$(date +%y-%m-%d)
 if [ -f "$LOCKFILE" ] ; then
     timestamp=$(date +%T)
 	echo "$timestamp > batch_pdf is already running" >> ~/phpcli_running.$d.log
 	exit 0
 else
     touch $LOCKFILE
-	d=$(date +%y-%m-%d)
 	/usr/bin/php ~/fivestone/cli/batch_pdf_aws.php >> ~/phpcli.$d.log 2> ~/phpcli_error.$d.log
     rm $LOCKFILE
 	exit 0
